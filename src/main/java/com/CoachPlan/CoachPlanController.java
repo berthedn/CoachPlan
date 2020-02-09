01ae2b0ea65c6129a9b90ee78ed7f3c3863efa95
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-09 15:22:00 -0500

    Update pom.xml / Creation index / Controller

---
+++ src/main/java/com/CoachPlan/CoachPlanController.java
@@ -0,0 +1,14 @@
+package com.CoachPlan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CoachPlanController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+}
