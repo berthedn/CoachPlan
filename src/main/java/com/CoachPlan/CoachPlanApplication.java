a23855dd5e4d9c3efd1b76480a8b62d48a67530a
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-09 15:02:53 -0500

    First Commit

---
+++ src/main/java/com/CoachPlan/CoachPlanApplication.java
@@ -0,0 +1,13 @@
+package com.CoachPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachPlanApplication.class, args);
+	}
+
+}
