5330a29fc1e14d98ecd3b373341f4d814160b6dd
Administrator <Administrator@DESKTOP-099UQRG>
2020-03-25 11:29:28 -0400

    Fixed the problems with login()

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,24 +1,10 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-	apiKey: "AIzaSyAi82LSIrKJhE-Hi2lDij_ATDq5yD52mIk",
-	authDomain: "localhost",
-	databaseURL: "https://coachplan-bcb7e.firebaseio.com",
-	projectId: "coachplan-bcb7e",
-	storageBucket: "coachplan-bcb7e.appspot.com",
-	messagingSenderId: "975486007303",
-	appId: "1:975486007303:web:208eae0def4e2fad1ed2a2",
-	measurementId: "G-ZBG24TF978"
-};
-	  
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 var currentEmail;
 var currentUserId;	 
 
-firebase.auth().onAuthStateChanged(function(user) {
+/*firebase.auth().onAuthStateChanged(function(user) {
 	console.log(user);
-})
+})*/
 
 function login() {
 	
@@ -26,6 +12,46 @@ function login() {
 	var userPass = document.getElementById("password_field").value;
 	
 	console.log(userEmail + " " + userPass);
+	
+	window.alert("Signed In");
+	currentEmail = userEmail.toString();
+	//changePage();
+	
+	/*firebase.firestore().collection('Users').get().then((snapshot) => {
+		snapshot.docs.forEach(doc => {
+			console.log(doc.data());
+			if(doc.id == currentEmail) {
+				console.log(doc.data());
+				currentUserId = doc.data().ID;
+			}
+		})
+	})*/
+	
+	var docRef = firebase.firestore().collection("Users").doc(currentEmail)
+	
+	docRef.get().then(function(doc){
+		if(doc.exists) {
+			console.log(doc.data());
+			currentUserId = doc.data().ID;
+			console.log(currentUserId);
+			
+			if(currentUserId == 1) {
+				location.replace("http://127.0.0.1:8080/athleteList");
+				console.log("1");
+			}else if(currentUserId == 2) {
+				location.replace("http://127.0.0.1:8080/athlete");
+				console.log("2");
+			} else {
+				location.replace("http://127.0.0.1:8080/index");	
+				console.log("3");
+			}
+		}else {
+			console.log("No such document");
+		}
+	}).catch(function(error) {
+		console.log("Error getting document:", error);
+	});
+	
 		
 	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
 		  // Handle Errors here.
@@ -36,18 +62,18 @@ function login() {
 		  window.alert("Error: " + errorMessage);
 		});
 	
-		window.alert("Signed In");
+		/*window.alert("Signed In");
 		currentEmail = userEmail.toString();
-		changePage();
-	
-	
+		changePage();*/
+		
 }
 
 function signOut(){
     
 	firebase.auth().signOut();
     alert("Signed Out");
-    changePage();
+    location.replace("http://127.0.0.1:8080/index");
+    //changePage();
 }
 
 function changePage() {
@@ -63,6 +89,7 @@ function changePage() {
 		  
 		  if (user) {
 			    // User is signed in
+			  
 				if(currentUserId == 1) {
 					location.replace("http://127.0.0.1:8080/athleteList");
 					console.log("1");
